517866be3f5c90be42dce119cb1b1f72f9081670
LuckyToxic <mrbalumba27@gmail.com>
2023-12-23 20:00:16 +0300

    New

---
+++ script.js
@@ -0,0 +1,97 @@
+
+/* let num1 = prompt('Введите ваше число:',5);
+
+let result = num1 **2
+
+alert(`Квадрат вашего числа равен: ${result}`)  */
+
+
+
+
+
+/* let num1 = Number(prompt('Введите ваше первое число:'))
+
+let num2 = Number(prompt('Введите ваше второе число:'))
+
+let result = (num1 + num2)/2
+
+alert(`Среднее арифметическое число: ${result}`) */ 
+
+
+
+
+
+
+/* let long = prompt('Ввидите длину стороны квадрата:')
+
+let result = long * long
+
+alert(`Площадь вашего квадрата равно: ${result}`) */
+
+
+
+
+/* let km = prompt('Введите ваше значение в киллометрах:')
+
+const result = km * 0.621371
+
+alert(`Ваше значение в милях будет равно: ${result}`) */
+
+
+
+
+/* let num1 = Number(prompt('Введите ваше первое число:'))
+
+let znak = prompt('Введите ваш знак для вычислений:')
+
+let num2 = Number(prompt('Ввидите ваше второе число:'))
+
+switch (znak) {
+    case '+':
+        result = num1 + num2
+        alert(`Резльутат: ${result}`)
+        break;
+    case '-':
+        result = num1 - num2
+        alert(`Результат: ${result}`)
+        break;
+    case '*':
+        result = num1 * num2
+        alert(`Результат: ${result}`)
+        break;
+    case '/':
+        result = num1 / num2
+        alert(`Результат: ${result}`)
+        break;        
+    default:
+        alert("Ты написал что-то не так");
+        
+} */
+
+
+
+
+
+/* 
+let a = Number(prompt('Ввидите ваше значение "a" для формулы a*x+b=0 :'))
+
+let b = Number(prompt('Ввидите ваше значение "b" для формулы a*x+b=0 :'))
+
+const x = -b/a
+
+alert(`В этой формуле "x" равен: ${x}`) */
+
+
+
+
+
+
+/* let hours = Number(prompt('Введите количество часов на данный момент:'))
+
+let minuts = Number(prompt('Ввидите количество минут на данный момент:'))
+
+let resultHours = (hours<24) ? 24 - hours : 'В сутках всего 24 часа!'
+
+let resultMinuts = (minuts<60) ? 60 - minuts : 'В одном часе всего 60 минут!'
+
+alert(`До нового дня осталось ${resultHours}чч:${resultMinuts}мм `) */
